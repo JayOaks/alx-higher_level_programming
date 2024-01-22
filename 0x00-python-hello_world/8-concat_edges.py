d3a9fafb902d78079ead765b874610bce0bce0e3
Jeroldine Oakley <oakleyjeroldine@gmail.com>
2024-01-22 05:39:20 -0500

    Update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-<<<<<<< HEAD
-    language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6]
-=======
  language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[105:110] + str[:6]
->>>>>>> 15a087c2df6c0dd08327cef75cd13af6569288c4
 print(str)
